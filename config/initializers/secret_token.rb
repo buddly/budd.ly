fb5f4e215acd003afe3388741da1a14a1afaabac
Jeff Emminger <jemminger@gmail.com>
2011-05-20 20:18:33 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buddly::Application.config.secret_token = 'dd409bb50ea21566d11331d3be916985ea47ce1bbb24fbbe383f516e8235cf5c828a79f51c2cb961e6f16f0aedd24d788336b5165385736508317d1dcc75a62a'
